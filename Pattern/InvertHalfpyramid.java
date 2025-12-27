066ff31a2aa42891910e803f51aea655f3fe09f5
shraddha <shraddhakapurkar167@gmail.com>
2025-12-27 11:59:32 +0530

    Invert Half Pyramid

---
+++ Pattern/InvertHalfpyramid.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class InvertHalfpyramid
+{  
+ public static void main(String args[])
+ {
+   int i,j;
+     for(i=1;i<=5;i++)
+	 {
+	   for(j=1;j<=5;j++)
+	   {
+	     if(j>=i)
+		 {
+		  System.out.print("*");
+		 
+		  }
+		   else
+		    {
+		     System.out.print("");
+		     }
+	    }
+     System.out.println(" ");
+	  }
+    }
+		   
+ }
